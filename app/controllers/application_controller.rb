324b64005d594d15cb9b5956c19525b2e49922d1
“Xabier <xabierhh@gmail.com>
2026-02-17 10:43:28 -0600

    feat: add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :authenticate_poc
+
+  private
+
+  def authenticate_poc
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["POC_USER"] && password == ENV["POC_PASS"]
+    end
+  end
 end
